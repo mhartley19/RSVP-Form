c1dc0a8c59fe73d1266f6af7d19c70e3fc9f3a2e
Mike Hartley <mhartley19@gmail.com>
2020-09-23 22:02:20 -0500

    I REALLY REALLY HATE THIS ASSIGNMENT

--- app.js
+++ app.js
@@ -9,53 +9,69 @@ const { v4: uuidv4 } = require('uuid')
 
 
 
-
+//WHY IN THE $%^() DOES FILENAME NOT WORK AND WTF IS THE BUFFER?????>??
 
 
 const startServer = () => {
-    const PORT_TO_USE = process.env.PORT || 4000
+    const PORT_TO_USE = process.env.PORT || 3000
     const app = express()
-    app.use(express.static('./public'))
     app.use(express.json())
     app.use(express.urlencoded({ extended: false}))
+    // app.use('/public',express.static(uploadDirectory))
     
-    
-
-    const uploadDirectory = path.resolve(__dirname, "public/uploads")
-
-    console.log(`The directory is ${uploadDirectory}`)
-
     const storageEngine = multer.diskStorage({
         destination: (req, file, callback) =>{
             callback(null, uploadDirectory)
         },
-        filename: function(req, file, callback){
-            const fileName =`${uuidv4}${path.extname(file.originalname)}`
+        filename: (req, file, callback) => {
+            const fileName = file.fieldname + Date.now() + path.extname(file.originalname)
+            console.log(fileName)
             callback(null, fileName)
         }
     })
 
+    const uploadDirectory = path.resolve(__dirname, 'public/uploads')
+
+    console.log(`The directory is ${uploadDirectory}`)
+
+
+    console.log(storageEngine)
+  
    const uploader = multer({ storageEngine })
     
 
     app.get('/', (req,res)=>{
-        const path = './public/uploads';
+        const path = './public/upload';
         fs.readdir(path, function(err, items) {
         
-        res.send(`<h1>Welcome to Kenziegram!</h1>
+        res.send(`
+        <!DOCTYPE html>
+                <html lang="en">
+                        <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Document</title>
+                        </head>
+                            <body>
+                            <h1>Welcome to Kenziegram!</h1>
     
-        <form action="/upload" method="POST" enctype="multipart/form-data">
-            <div class="file-field input-field">
-              <div class="btn">
-                <span>File</span>
-                <input type="file" name="myImage">
-              </div>
-              <div class="file-path-wrapper">
-                <input class="file-path validate" type="text">
-              </div>
-            </div>
-            <button type="submit" class="btn">Submit</button>
-          </form>
+                            <form action="/upload" method="POST" enctype="multipart/form-data">
+                                <div class="file-field input-field">
+                                  <div class="btn">
+                                    <span>File</span>
+                                    <input type="file" name="myImage">
+                                  </div>
+                                  <div class="file-path-wrapper">
+                                    <input class="file-path validate" type="text">
+                                  </div>
+                                </div>
+                                <button type="submit" class="btn">Submit</button>
+                              </form>
+                            </body>
+                            </html>
+        
+        
+       
         
         `)
         })
@@ -65,9 +81,10 @@ const startServer = () => {
     app.post('/upload', uploader.single('myImage'), function (req,res, next) {
         console.log("hello")
         res.send(req.file)
+        console.log(req.file)
     })
        
-         
+ 
     
     app.listen(PORT_TO_USE, () => console.log(`Server loaded on port ${PORT_TO_USE}`))
 }
